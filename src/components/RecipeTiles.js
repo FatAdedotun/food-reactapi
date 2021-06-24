02b4ca40041c8899194be5196829015e81b67d07
Adedotun Fatokun <fatokun.adedotun@gmail.com>
2021-06-24 16:13:56 +0100

    alt

--- src/components/RecipeTiles.js
+++ src/components/RecipeTiles.js
@@ -6,7 +6,7 @@ const RecipeTiles = ({ recipe }) => {
         recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) 
         != null && (
             <div className="recipeTile">
-                <img className="recipeTile__img" src={recipe["recipe"]["image"]} />
+                <img className="recipeTile__img" src={recipe["recipe"]["image"]} alt=""/>
                 <p className="recipeTile__name">{recipe["recipe"]["label"]}</p>
             </div>
         )

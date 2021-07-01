4800a4c8d87671b2c8ff613d7537cf5089c4ce60
Adedotun Fatokun <fatokun.adedotun@gmail.com>
2021-07-01 12:01:03 +0100

    fragments

--- src/App.js
+++ src/App.js
@@ -26,43 +26,45 @@ function App() {
 
 
   return (
-    <div className="app">
-      <h1>Food Recipe Empire</h1>
+    <>
+      <div className="app">
+        <h1>Food Recipe Empire</h1>
 
-      <form className="app__searchForm" onClick={onSubmit}>
-        <input type="text" 
-        className="app__input"
-        placeholder="enter ingredients" 
-        value={query} 
-        onChange={(e) => setQuery(e.target.value)} />
-        <input type="submit"
-        className="app__submit"
-        value="Search" />
+        <form className="app__searchForm" onClick={onSubmit}>
+          <input type="text" 
+          className="app__input"
+          placeholder="enter ingredients" 
+          value={query} 
+          onChange={(e) => setQuery(e.target.value)} />
+          <input type="submit"
+          className="app__submit"
+          value="Search" />
 
-        <select className="app__healthLabels">
-          <option value="vegan" onClick={() => sethealthLabels("vegan")}>Vegan</option>
-          <option value="Vegetarian" onClick={() => sethealthLabels("Vegetarian")}>Vegetarian</option>
-          <option value="soy-free" onClick={() => sethealthLabels("soy-free")}>soy-free</option>
-          <option value="tree-nut-free" onClick={() => sethealthLabels("tree-nut-free")}>tree-nut-free</option>
-          <option value="peanut-free" onClick={() => sethealthLabels("peanut-free")}>peanut-free</option>
-          <option value="egg-free" onClick={() => sethealthLabels("egg-free")}>egg-free</option>
-          <option value="low-sugar" onClick={() => sethealthLabels("low-sugar")}>low-sugar</option>
-          <option value="dairy-free" onClick={() => sethealthLabels("dairy-free")}>dairy-free</option>
-          <option value="glutten-free" onClick={() => sethealthLabels("glutten-free")}>glutten-free</option>
-          <option value="wheat-free" onClick={() => sethealthLabels("wheat-free")}>wheat-free</option>
-          <option value="paleo" onClick={() => sethealthLabels("paleo")}>paleo</option>
-        </select>
-      </form>
+          <select className="app__healthLabels">
+            <option value="vegan" onClick={() => sethealthLabels("vegan")}>Vegan</option>
+            <option value="Vegetarian" onClick={() => sethealthLabels("Vegetarian")}>Vegetarian</option>
+            <option value="soy-free" onClick={() => sethealthLabels("soy-free")}>soy-free</option>
+            <option value="tree-nut-free" onClick={() => sethealthLabels("tree-nut-free")}>tree-nut-free</option>
+            <option value="peanut-free" onClick={() => sethealthLabels("peanut-free")}>peanut-free</option>
+            <option value="egg-free" onClick={() => sethealthLabels("egg-free")}>egg-free</option>
+            <option value="low-sugar" onClick={() => sethealthLabels("low-sugar")}>low-sugar</option>
+            <option value="dairy-free" onClick={() => sethealthLabels("dairy-free")}>dairy-free</option>
+            <option value="glutten-free" onClick={() => sethealthLabels("glutten-free")}>glutten-free</option>
+            <option value="wheat-free" onClick={() => sethealthLabels("wheat-free")}>wheat-free</option>
+            <option value="paleo" onClick={() => sethealthLabels("paleo")}>paleo</option>
+          </select>
+        </form>
 
-      <div className="app__recipes">
-        {recipes.map(recipe => {
-          return <RecipeTiles recipe={recipe}/>
-        })}
+        <div className="app__recipes">
+          {recipes.map(recipe => {
+            return <RecipeTiles recipe={recipe}/>
+          })}
+        </div>
       </div>
       <div className="FooterText">
         <p><a href="https://fatadedotun.netlify.app/">Adedotun Fatokun.</a> All rights reserved &copy; 2021.</p>
       </div>
-    </div>
+    </>
   );
 }
 
